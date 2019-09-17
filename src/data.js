35aca78f0e4ac2528c919a277305af40df1a185e
Johnny Lo <johnny.lo@scmp.com>
2019-09-17 16:05:14 +0800

    Update README.md

--- src/data.js
+++ src/data.js
@@ -3,97 +3,116 @@ export default {
     {
       id: 1,
       text: 'Hong Kong’s overtime work culture is a time bomb that may explode if left unchecked.',
-      topics: ['Hong Kong']
+      topics: ['Hong Kong'],
+      timeStamp: '2019-09-15T00:00:00.000Z'
     },
     {
       id: 2,
       text: 'Magic, Cantopop, a French author, and the Hong Kong performer looking to recreate an age of innocence in her shows.',
-      topics: ['Music']
+      topics: ['Music'],
+      timeStamp: '2019-09-13T00:00:00.000Z'
     },
     {
       id: 3,
       text: 'Hong Kong musician becomes the first Asian nominee for the distinguished UK classical music magazine Gramophone’s 2019 Orchestra of the Year Award.',
-      topics: ['Music']
+      topics: ['Music'],
+      timeStamp: '2019-09-11T00:00:00.000Z'
     },
     {
       id: 4,
       text: 'The richest man calls for peace Hong Kong, adding his voice to the chorus of peace.',
-      topics: ['Hong Kong']
+      topics: ['Hong Kong'],
+      timeStamp: '2019-09-10T00:00:00.000Z'
     },
     {
       id: 5,
       text: 'Shenzhen needs to attract overseas talent to come Greater Bay Area.',
-      topics: ['China']
+      topics: ['China'],
+      timeStamp: '2019-09-10T00:00:00.000Z'
     },
     {
       id: 6,
       text: 'Where a fun loving person eats in China',
-      topics: ['China']
+      topics: ['China'],
+      timeStamp: '2019-09-10T00:00:00.000Z'
     },
     {
       id: 7,
       text: 'Hong Kong property investors ignore Singapore in favour of cheaper options elsewhere',
-      topics: ['Asia']
+      topics: ['Asia'],
+      timeStamp: '2019-09-08T00:00:00.000Z'
     },
     {
       id: 8,
       text: 'Indonesia picks area on Borneo for new capital, with relocation estimated to cost US$33 billion',
-      topics: ['Asia']
+      topics: ['Asia'],
+      timeStamp: '2019-09-07T00:00:00.000Z'
     },
     {
       id: 9,
       text: 'US and China need to work together for the world to benefit from the digital era.',
-      topics: ['China']
+      topics: ['China'],
+      timeStamp: '2019-09-06T00:00:00.000Z'
     },
     {
       id: 10,
       text: 'North Korea launches fresh missiles into sea off Japan.',
-      topics: ['Asia']
+      topics: ['Asia'],
+      timeStamp: '2019-09-05T00:00:00.000Z'
     },
     {
       id: 11,
       text: 'Staffs in a Hong Kong airlines face unpaid leave or reduced working hours.',
-      topics: ['Hong Kong']
+      topics: ['Hong Kong'],
+      timeStamp: '2019-09-05T00:00:00.000Z'
     },
     {
       id: 12,
       text: 'China again blocks US Navy port visit as Qingdao request is denied.',
-      topics: ['Asia']
+      topics: ['Asia'],
+      timeStamp: '2019-09-04T00:00:00.000Z'
     },
     {
       id: 13,
       text: 'Journalists wear safety gear to Hong Kong police press conference.',
-      topics: ['Hong Kong']
+      topics: ['Hong Kong'],
+      timeStamp: '2019-09-04T00:00:00.000Z'
     },
     {
       id: 14,
       text: 'What is prorogation? A simple guide to what is happening in UK politics.',
-      topics: ['Eurpoe']
+      topics: ['Eurpoe'],
+      timeStamp: '2019-09-04T00:00:00.000Z'
     },
     {
       id: 15,
       text: 'Has UK Prime Minister Boris Johnson talked to the someone?',
-      topics: ['Europe']
+      topics: ['Europe'],
+      timeStamp: '2019-09-04T00:00:00.000Z'
     },
     {
       id: 16,
       text: 'Northern Ireland court dismisses case against no-deal Brexit.',
-      topics: ['Europe']
+      topics: ['Europe'],
+      timeStamp: '2019-09-04T00:00:00.000Z'
     },
     {
       id: 17,
       text: 'Scientists discover a big planet that’s potentially habitable.',
-      topics: ['Europe']
+      topics: ['Europe'],
+      timeStamp: '2019-09-04T00:00:00.000Z'
     },
     {
       id: 18,
       text: 'Never lose your glasses again: why a Hong Kong filmmaker is bringing back the eyewear chain.',
-      topics: ['Fashion']
+      topics: ['Fashion'],
+      timeStamp: '2019-09-04T00:00:00.000Z'
     },
     {
       id: 19,
       text: 'Crazy Rich Asian wedding planner on million dollar dresses.',
-      topics: ['Asia']
+      topics: ['Asia'],
+      timeStamp: '2019-09-03T00:00:00.000Z'
     }
   ],
   topics: [
